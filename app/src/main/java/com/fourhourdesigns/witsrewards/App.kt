143f2f02cbb405bfa5f22ddb81a08b6f0c42ebfd
nicferretti <ferrettinicholas@gmail.com>
2019-03-26 16:39:23 +0200

    Added LocationHelper to easily get device current location from gps

---
+++ app/src/main/java/com/fourhourdesigns/witsrewards/App.kt
@@ -0,0 +1,17 @@
+package com.fourhourdesigns.witsrewards
+
+import android.app.Application
+import android.content.Context
+
+class App: Application() {
+
+    companion object {
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        context = this
+    }
+}
